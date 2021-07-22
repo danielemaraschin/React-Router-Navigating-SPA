8fa833a1a81e9c52f8084300fabbd7b9454228c7
danielemaraschin <danikmaraschin@gmail.com>
2021-07-22 20:35:06 +0200

    working with error 404

---
+++ petshop/src/paginas/Pagina404.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import imagem from '../assets/img/doguito404.svg'
+const Pagina404 = () => {
+    return (
+        <main>
+            <img src={imagem} alt=" "/>
+            <p>
+                Ops, page not found!
+            </p>
+        </main>
+    )
+}
+
+export default Pagina404
