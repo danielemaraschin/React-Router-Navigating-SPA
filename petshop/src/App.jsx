a403ecad01cfa5ba7e24c80fc5ae4b321b979a37
danielemaraschin <danikmaraschin@gmail.com>
2021-07-22 12:02:40 +0200

    Learning about React Virtual Dom

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './assets/css/base/base.css'
 import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
 
 function App() {
-  const Router = () => {
-    const location = window.location.pathname
-    if(location === '/sobre'){
-      return <Sobre/> // se for no endereco '/sobre' vai para a pagina sobre.js
-    }else{
-      return <Home /> //se for para qualquer outro endereco vai pra Home.js
-    }
-  }
+
   return (
-  <>
-    {Router ()}
-  </>
+    <Router>
+      <Route>
+        <Home />
+      </Route>
+      <Route>
+        <Sobre />
+      </Route>
+    </Router>
   )
 }
 
