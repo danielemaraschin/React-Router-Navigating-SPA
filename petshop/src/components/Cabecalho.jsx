effa52269fa5d3e9c64671ab07e48b0b3c45596c
danielemaraschin <danikmaraschin@gmail.com>
2021-07-22 21:56:09 +0200

    Learning about the difference between <a> and <Link>

---
+++ petshop/src/components/Cabecalho.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import imagem from '../assets/img/doguito.svg'
+import '../assets/css/componentes/cabecalho.css'
+const Cabecalho = () => {
+    return(
+        <header className="cabecalho container">
+            <div className="menu-hamburguer">
+                <span className="menu-hamburguer__icone"></span>
+            </div>
+            <div className="cabecalho-container">
+                <Link to="/" className="flex flex--centro">
+                    <img className="cabecalho__logo" src={imagem} alt="Logo Doguito"/>
+                    <h1 className="cabecalho__titulo"> Pet Shop</h1>
+                </Link>
+            </div>
+            <nav className ="menu-cabecalho">
+                <ul className="menu-itens">
+                    <li><a href="#" className="menu-item menu-item--entrar" >Entrar</a></li>
+                    <li><a href="#"  className="menu-item"> Produtos</a></li>
+                    <li><a href="#"  className="menu-item"> Blog</a></li>
+                    <li>< Link to="/sobre"  className="menu-item"> Sobre</Link></li>
+                </ul>
+            </nav>
+            <div className="menu-cabecalho-background"></div>
+        </header>
+    )
+        
+}
+
+export default Cabecalho
