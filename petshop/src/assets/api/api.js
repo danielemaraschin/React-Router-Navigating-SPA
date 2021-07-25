099c7f123a14dd1c939ccbf559cbe78105283fdf
danielemaraschin <danikmaraschin@gmail.com>
2021-07-25 10:31:31 +0200

    working with async await

--- petshop/src/assets/api/api.js
+++ petshop/src/assets/api/api.js
@@ -3,3 +3,8 @@ import axios from 'axios'
 export const api = axios.create({
     baseURL: 'http://localhost:5000'
 })
+
+export const busca = async () => {
+    const resposta = await api.get(url)
+    setDado(resposta.data)
+}
