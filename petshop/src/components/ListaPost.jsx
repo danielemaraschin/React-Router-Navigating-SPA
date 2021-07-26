887ef2f922241e1fdf646e8f1841cf0d1bd50368
danielemaraschin <danikmaraschin@gmail.com>
2021-07-26 12:45:05 +0200

    working with useEffect

--- petshop/src/components/ListaPost.jsx
+++ petshop/src/components/ListaPost.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import {busca} from '../assets/api/api'
 
-const ListaPost = () => {
-    return(
-        <section className="posts container">
+const ListaPost = ({url}) => {
 
+    const [post, setPosts] = useState([])
+
+    useEffect(() =>{
+        busca(url,setPosts)
+    }, [])
+    
+    
+    return(
+        <section>
+            {
+                post.map((post) => (
+                    <Link className={`cartao-post cartao-post--${post.categoria}`}>
+                        <article key={post.id}>
+                            <h3 className="cartao-post__titulo">{post.title}</h3>
+                            <p className="cartao-post__meta">{post.metadescription}</p>
+                        </article>
+                    </Link>
+                ))
+            }
         </section>
     )
 }
