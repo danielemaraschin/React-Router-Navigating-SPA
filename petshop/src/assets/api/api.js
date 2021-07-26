9767a56fe087be154afbd664f30e694d8d9b866d
danielemaraschin <danikmaraschin@gmail.com>
2021-07-26 12:29:59 +0200

    working with json-server

--- petshop/src/assets/api/api.js
+++ petshop/src/assets/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'http://localhost:5000' //url base da api
 })
 
 export const busca = async () => {
