b2385d0d65386656850a786e80ec6fb6ff4399c8
danielemaraschin <danikmaraschin@gmail.com>
2021-07-27 14:49:32 +0200

    redirecting the page with <link>

--- petshop/src/components/ListaPost.jsx
+++ petshop/src/components/ListaPost.jsx
@@ -15,7 +15,7 @@ const ListaPost = ({url}) => {
         <section className="posts container">
             {
                 post.map((post) => (
-                    <Link className={`cartao-post cartao-post--${post.categoria}`}>
+                    <Link className={`cartao-post cartao-post--${post.categoria}`} to={`/posts/${post.id}`}>
                         <article key={post.id}>
                             <h3 className="cartao-post__titulo">{post.title}</h3>
                             <p className="cartao-post__meta">{post.metadescription}</p>
