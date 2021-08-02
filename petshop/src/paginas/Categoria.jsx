f0d6ca0a7471540e7fbf355cadac5bfea6be1752
danielemaraschin <danikmaraschin@gmail.com>
2021-08-02 14:18:12 +0200

    Nested routes : working with link to and .map

--- petshop/src/paginas/Categoria.jsx
+++ petshop/src/paginas/Categoria.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Route, useParams, useRouteMatch } from 'react-router-dom'
+import { Route, useParams, useRouteMatch, Link } from 'react-router-dom'
 import '../assets/css/blog.css'
 import ListaCategorias from '../components/ListaCategorias'
 import ListaPost from '../components/ListaPost'
@@ -7,7 +7,8 @@ import { busca } from '../assets/api/api'
 
 const Categoria = () => {
     const { id } = useParams()
-    const { path } = useRouteMatch()
+    const { url,  path } = useRouteMatch()
+    console.log(url)
     const [subcategorias, setSubcategorias] = useState([])
     //efeito colateral de atualizacao de componentes de api
     useEffect(() => {       //poderiamos colcoar o setSubcategorias, mas vamos colocar uma funcao anonima q recebe como parametro categoria e então executa setSubcategoria
@@ -24,9 +25,15 @@ const Categoria = () => {
             <ListaCategorias /> {/*Dentro das categorias tem os posts */}
             <ul className="lista-categorias container flex">
                 {
-                <li className={`lista-categorias__categoria lista-categorias__categoria--${id}`} key={subcategorias}>
+                    subcategorias.map((subcategoria) => (
+                        <li className={`lista-categorias__categoria lista-categorias__categoria--${id}`} key={subcategoria}>
+                            <Link to={`${url}/${subcategoria}`}>
+                                {subcategoria} {/*aqui  vai pegar o nome dessa subcategoria*/}
+                            </Link>
 
-                </li>
+                        </li>
+                    ))
+
                 }
             </ul>
             <Route exact path={`${path}/`}> {/*vai mostrar em qual caminho esta, wellness ou comportamento, conforme clicamos em cima */}
